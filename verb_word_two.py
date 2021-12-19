583b38d0203af0a4869f7ae64d6b95f64c36faf6
ViggoMode210 <86791607+ViggoMode2021@users.noreply.github.com>
2021-12-19 17:47:15 -0500

    Update verb_word_two.py

--- verb_word_two.py
+++ verb_word_two.py
@@ -3,6 +3,7 @@ from verb_world import *
 
 experience_wallet = 75
 
+
 def log_in(name, occupation, password):
     print("In order to proceed to the next level, you will need to log in again.")
     while True:
@@ -28,6 +29,7 @@ def log_in(name, occupation, password):
         else:
             print("Incorrect, please try again.")
 
+
 def verb_questions_two():
     while True:
         scramble_aprender = "aprender"
@@ -37,14 +39,56 @@ def verb_questions_two():
         print(f'Unscramble the following verb "{y}"')
         to_learn = input(
             '¿Cómo se dice "to learn" en español? \n Please unscramble the verb. ')
-        if to_learn== 'aprender':
+        if to_learn == 'aprender':
             print('Excelente, "to learn" es "aprender" en español.')
             verbs_learned['aprender'] = 'to learn'
             verbs_to_learn.pop('to learn')
             print("Here is your updated dictionary of verbs you learned!", verbs_learned)
             global experience_wallet
             experience_wallet += 20
-            print(f"You earned 20 points. You now have {experience_wallet} in your experience wallet.")
+            print(
+                f"You earned 20 points. You now have {experience_wallet} in your experience wallet.")
             break
         else:
-            print('Intenta otra vez, por favor.', to_learn, 'is not "to learn."')
+            print('Intenta otra vez, por favor.',
+                  to_learn, 'is not "to learn."')
+
+    while True:
+        scramble_trabajar = "trabajar"
+        l = list(scramble_trabajar)
+        random.shuffle(l)
+        y = ''.join(l)
+        print(f'Unscramble the following verb "{y}"')
+        to_work = input(
+            '¿Cómo se dice "to work" en español? \n Please unscramble the verb. ')
+        if to_work == 'trabajar':
+            print('Por supuesto, "to work" es "trabajar" en español.')
+            verbs_learned['trabajar'] = 'to work'
+            verbs_to_learn.pop('to work')
+            print("Here is your updated dictionary of verbs you learned!", verbs_learned)
+            experience_wallet += 20
+            print(
+                f"You earned 20 points again, you got money like that?. You now have {experience_wallet} in your experience wallet.")
+            break
+        else:
+            print('Intenta otra vez, por favor.', to_work, 'is not "to work."')
+
+    while True:
+        scramble_vivir = "vivir"
+        l = list(scramble_vivir)
+        random.shuffle(l)
+        y = ''.join(l)
+        print(f'Unscramble the following verb "{y}"')
+        to_live = input(
+            '¿Cómo se dice "to live" en español? \n Please unscramble the verb. ')
+        if to_live == 'vivir':
+            print('Genial, "to live" es "vivir" en español.')
+            verbs_learned['vivir'] = 'to live'
+            verbs_to_learn.pop('to live')
+            print("Here is your updated dictionary of verbs you learned!", verbs_learned)
+            experience_wallet += 20
+            print(
+                f"You earned another whopping 20 points. You now have {experience_wallet} in your experience wallet.")
+            break
+        else:
+            print('Intenta otra vez, por favor.', to_live, 'is not "to live."')
