28ac98dbb72f66d8ff7818f92cafa76b828c47bd
ViggoMode210 <86791607+ViggoMode2021@users.noreply.github.com>
2021-12-21 19:38:36 -0500

    Update prizes.py

--- prizes.py
+++ prizes.py
@@ -10,21 +10,18 @@ def travel_selection(travel_prizes):
       global experience_wallet
       print(f'You have {experience_wallet} in your experience wallet. Where would you like to purchase your trip to? ')
 
-      prize_selector = int(input("1. Press 1 if you want to save your experience. 2. Press 2 to choose your own from the list."))
+      prize_selector = int(input("1. Press 1 if you want to save your experience. 2. Press 2 to choose your own from the list. "))
 
       while True:
             if prize_selector == 1:
                   print("Okay, we hope to see you in the next game!")
                   break
-      else:
-            print("Please type either 1 or 2 to send a valid response!")
 
-      while True:
             if prize_selector == 2:
                   prize_selector_choose = input('Where would you like to go? ')
                   if prize_selector_choose == 'Argentina':
                         experience_wallet -= 200
-                        print(f'You now have {experience_wallet} in your experience wallet. Fun fact: Argentina has the most european ancestary of any Latin American country.')
+                        print(f'You now have {experience_wallet} in your experience wallet. Fun fact: Argentina has the most European ancestary of any Latin American country.')
                         break
                   if prize_selector_choose == 'Colombia':
                         experience_wallet -= 170
@@ -38,6 +35,7 @@ def travel_selection(travel_prizes):
                         experience_wallet -= 180
                         print(f'You now have {experience_wallet} in your experience wallet. Fun fact: "Quito is the second highest capital city in terms of elevation (9350 ft above sea level)')
                         break
+                      
                   if prize_selector_choose == 'Peru':
                         experience_wallet -= 180
                         print(f'You now have {experience_wallet} in your experience wallet. Fun fact: Huacachina is the only natural oasis in South America.')
